a96e2f384a4bcbd0aa699435b9d1031e37d0ce09
Erkan Akkoç <akkoc.erkan@gmail.com>
2021-01-24 06:04:41 +0300

    D5 Updates - OOP2 & OOP3

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //Interface olduğunu belli etmek için I ile başlatırız
+    interface IKrediManager
+    {
+        void Hesapla();
+
+        void BiseyYap();
+    }
+}
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
